e162bb87a07ae9162958a9dff4fd8d99240112e8
ReyRachel8L <rlim.rachel08@gmail.com>
2022-07-09 10:43:49 +0800

    Initial Commit

---
+++ SAP Friends App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SAP Friends App
+//
+//  Created by LIM YEE SIN RACHEL stu on 9/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
